60056359c2ce67b56a52dc7ac7410ae09429f9c9
Srihari <sriharikaka@gmail.com>
2022-09-15 11:30:37 +0530

    updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index():
-    return "Starting Machine Learning Project!"
+    return "CICD pipeline has been established!"
 
 if __name__ == "__main__":
     app.run(debug=True)
