bdc2b82aa0bbc8ca0e1a3d7040f0910a13b69c4a
Srihari <sriharikaka@gmail.com>
2022-09-22 15:47:45 +0530

    pipeline has been established

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -78,7 +78,7 @@ class Configuration:
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         pass
 
-    def get_model_evaluation_config(self) ->ModelEvaluationConfig:
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         pass
 
     def get_model_pusher_config(self) -> ModelPusherConfig:
--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -1,17 +1,19 @@
 import logging
 from datetime import datetime
 import os
+from housing.constant import get_current_time_stamp
 
 LOG_DIR="housing_logs"
 
-CURRENT_TIME_STAMP=  f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+def get_log_file_name():
+    return f"log_{get_current_time_stamp()}.log"
 
-LOG_FILE_NAME=f"log_{CURRENT_TIME_STAMP}.log"
-
+LOG_FILE_NAME=get_log_file_name()
 
 os.makedirs(LOG_DIR,exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
+
 
 logging.basicConfig(filename=LOG_FILE_PATH,
 filemode="w",
